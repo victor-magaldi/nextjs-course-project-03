ff2ed1c6ab5d8371e34d710694b57f0dbd91ed8d
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-11-07 22:26:59 -0300

    feat: implement btn logout and validation login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import { auth } from "@/auth";
+
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <main>
+      {session?.user ? <p>Logado</p> : <p>Deslogado</p>}
       <form action={actions.signIn}>
-        <Button type="submit">Click Me</Button>
+        <Button type="submit" className="bg-green-600">
+          Sign In
+        </Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit" className="bg-gray-500">
+          Sign out
+        </Button>
       </form>
     </main>
   );
