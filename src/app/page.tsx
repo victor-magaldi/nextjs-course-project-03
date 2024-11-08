aaba9e005ec68145356136dfcc2f7956aed4f1b9
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-11-07 22:17:48 -0300

    feat: implement button login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
 
 export default function Home() {
   return (
     <main>
-      <Button>Click Me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Click Me</Button>
+      </form>
     </main>
   );
 }
