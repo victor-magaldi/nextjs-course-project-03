5f6227b25ab8c368d879e69423f1abff874b20fb
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-11-24 21:05:40 -0300

    feat: separete server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,5 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { createTopic } from "./create-topic";
+export { createPost } from "./create-post";
+export { createComment } from "./create-comment";
