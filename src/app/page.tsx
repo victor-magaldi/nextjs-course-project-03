b1e7a6ace1732bc086ae5efe87f7bb845754333c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-12-05 22:56:07 -0300

    feat: remobe resourse dynamic in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 
 import * as actions from "@/actions";
 import { Button } from "@nextui-org/react";
 import { Profile } from "@/components/profile";
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
+  // const session = await auth();
 
   return (
     <main>
-      <div>
-        Profile back-end
-        {session?.user ? <p>Logado</p> : <p>Deslogado</p>}
-      </div>
-      <form action={actions.signIn}>
-        <Button type="submit" className="bg-green-600">
-          Sign In
-        </Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit" className="bg-gray-500">
-          Sign out
-        </Button>
-      </form>
       <Profile />
     </main>
   );
