ca4ef1440048ed2c6f63053b61b9ecc6d62488f2
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-12-05 23:14:09 -0300

    feat: implement jsx createTopic

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,33 @@
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+
 export function TopicCreateForm() {
-  return <div>topic-create-form</div>;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
