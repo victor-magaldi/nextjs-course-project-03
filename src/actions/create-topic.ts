0f977e4406ee524ac96df920993d4a86febe9baa
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-12-08 20:06:41 -0300

    feat: init schema validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,7 +1,19 @@
 "use server";
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, "must be lowerccase letters or dashes without spaces"),
+  description: z.string().min(10),
+});
 
 export async function createTopic(formData: FormData) {
   const name = formData.get("name");
   const description = formData.get("descriptions");
-  console.log(name, description);
+
+  const result = createTopicSchema.safeParse({ name, description });
+
+  console.log("result", result);
 }
