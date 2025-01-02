dc4bdf975c014a044b4825ecb874dc5070677147
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-01 21:17:43 -0300

    feat: creat structur for display create post Form

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { PostCreateForm } from "@/components/posts/create-form";
 interface TopicShowPageProps {
   params: {
     slug: string;
@@ -11,7 +12,9 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
       </div>
-      <div></div>
+      <div>
+        <PostCreateForm />
+      </div>
     </div>
   );
 }
