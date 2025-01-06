a444286371bec64c550d256e51175653b635964b
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-06 20:10:34 -0300

    feat: implement post view

--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -1,9 +1,16 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
 interface PostShowProps {
-  title: string;
-  content: string;
+  postId: string;
 }
 
-export default function PostShow({ title, content }: PostShowProps) {
+export default async function PostShow({ postId }: PostShowProps) {
+  const post = await db.post.findFirst({
+    where: { id: postId },
+  });
+  if (!post) {
+    notFound();
+  }
   return (
     <div className="m-4">
       <h1 className="text-2xl font-bold my-2">{post.title}</h1>
