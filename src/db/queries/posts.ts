fb3e6b015f0389dc5e8c0a33d2e4732250ebc358
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-06 20:02:41 -0300

    feat: add example dynamic type

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -6,6 +6,9 @@ export type PostWithData = Post & {
   user: { name: string | null };
   _count: { comments: number };
 };
+// export type PostWithData = Awaited<
+//   ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
 
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
