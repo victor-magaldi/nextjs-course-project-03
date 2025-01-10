fd4e4d9c51869d145d8334a30fc7a8fcb5cfa8e5
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-10 19:59:41 -0300

    feat: implement request memoization in query DB

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,25 +1,26 @@
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 import { db } from "@/db";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  console.log("chamada db");
-  return db.comment.findMany({
-    where: {
-      postId,
-    },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("chamada db");
+    return db.comment.findMany({
+      where: {
+        postId,
+      },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
