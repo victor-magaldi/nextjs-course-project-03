29ec9def8ace647a0cc758ff9840e4bbdea4fc35
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-11 22:04:17 -0300

    feat: implement suspense (streaming component)

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -3,6 +3,7 @@ import PostShow from "@/components/posts/post-show";
 import { CommentList } from "@/components/comments/comment-list";
 import { CommentCreateForm } from "@/components/comments/comment-create-form";
 import { paths } from "@/paths";
+import { Suspense } from "react";
 
 interface PostShowPageProps {
   params: {
@@ -19,7 +20,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<div>loading...</div>}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
