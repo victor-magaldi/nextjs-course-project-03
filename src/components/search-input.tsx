a16ade6c7dfb154f23863d004958c92fa5d7e06d
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-13 20:06:59 -0300

    feat: add action search

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import * as actions from "@/actions";
 
 export function SearchInput() {
   const searchParams = useSearchParams();
   return (
-    <Input placeholder="buscar" defaultValue={searchParams.get("term") || ""} />
+    <form action={actions.search}>
+      <Input
+        placeholder="buscar"
+        id="term"
+        name="term"
+        defaultValue={searchParams.get("term") || ""}
+      />
+    </form>
   );
 }
