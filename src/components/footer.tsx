8b30b936f25d8eaa555a47a6eff056463eaa13fa
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2025-01-15 18:19:54 -0300

    fix: rename footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export async function Footer() {
     <footer className="bg-customGreen-dark text-white py-6">
       <div className="container mx-auto text-center">
         <p className="text-sm">
-          &copy; 2025 Seu Nome ou Projeto. Todos os direitos reservados.
+          &copy; 2025 Discuss. Todos os direitos reservados.
         </p>
       </div>
     </footer>
